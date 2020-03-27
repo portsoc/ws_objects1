8b400a75c0bcbe885567d32a98d80049b9fc077e
Rich Boakes <rjb@port.ac.uk>
2020-03-27 21:13:02 +0000

    full screen width

---
+++ stages/13/classes.mjs
@@ -0,0 +1,44 @@
+export class Shape {
+  constructor(x, y, col) {
+    this.x = x;
+    this.y = y;
+    this.col = col;
+  }
+
+  moveBy(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  moveTowards(x, y) {
+    this.x -= (this.x - x) / 2;
+    this.y -= (this.y - y) / 2;
+  }
+}
+
+export class Rectangle extends Shape {
+  constructor(x, y, width, height, col) {
+    super(x, y, col);
+    this.width = width;
+    this.height = height;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.col;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+export class Circle extends Shape {
+  constructor(x, y, r, col) {
+    super(x, y, col);
+    this.r = r;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.col;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
