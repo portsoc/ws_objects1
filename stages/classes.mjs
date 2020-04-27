b6247cc36ab742f3ddb0c9aa1bd5c992badbcf15
Jacek Kopecky <jacek@jacek.cz>
2020-04-27 20:53:32 +0100

    stage 14

--- stages/classes.mjs
+++ stages/classes.mjs
@@ -42,3 +42,37 @@ export class Circle extends Shape {
     ctx.fill();
   }
 }
+
+
+export class Tentacle {
+  makeCol(i) {
+    return `hsl(${i * 360 / this.num}deg, 100%, 50%)`;
+  }
+
+  constructor(x, y, num, size) {
+    this.x = x;
+    this.y = y;
+    this.num = num;
+    this.size = size;
+    this.shapes = [];
+
+    for (let i = 0; i < this.num; i += 1) {
+      this.shapes.push(new Circle(this.x, this.y, (this.num - i) * this.size, this.makeCol(i)));
+    }
+  }
+
+  draw(ctx) {
+    for (const s of this.shapes) {
+      s.draw(ctx);
+    }
+  }
+
+  moveTowards(x, y) {
+    let prev = { x, y };
+    for (let i = this.shapes.length - 1; i >= 0; i--) {
+      const s = this.shapes[i];
+      s.moveTowards(prev.x, prev.y);
+      prev = { x: s.x, y: s.y };
+    }
+  }
+}
