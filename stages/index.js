886e437dc4b6b3d2b27e0d51e0235890507af3cd
Jacek Kopecky <jacek@jacek.cz>
2020-04-27 20:53:32 +0100

    stage 11

--- stages/index.js
+++ stages/index.js
@@ -1,27 +1,25 @@
 'use strict';
-/**
- * Adding functions to a superclass makes them available
- * to all classes that extend it.
- *
- * Here, we use `moveBy()` on rectangles and circles
- * even though it is only defined on Shape.
- */
 
-import { Circle, Rectangle } from './classes.mjs';
+import { Circle } from './classes.mjs';
+
+function makeCol(i) {
+  return `hsl(${i * 360 / NUM}deg, 100%, 50%)`;
+}
 
 // create circle and rectangle objects
-const shapes = [
-  new Rectangle(100, 50, 100, 200, 'crimson'),
-  new Rectangle(300, 150, 100, 200, 'steelblue'),
-  new Circle(150, 350, 50, 'crimson'),
-  new Circle(350, 450, 50, 'steelblue'),
-];
+const shapes = [];
+
+const NUM = 18;
+const SIZE = 10;
+
+for (let i = 0; i < NUM; i += 1) {
+  shapes.push(new Circle(400, 400, (NUM - i) * SIZE, makeCol(i)));
+}
 
 // get a handle on the drawing canvas
 const ctx = document.querySelector('canvas').getContext('2d');
 
 for (const s of shapes) {
   s.draw(ctx);
-  s.moveBy(110, 30);
-  s.draw(ctx);
+  console.log(s.col);
 }
