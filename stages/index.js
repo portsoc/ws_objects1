e4091d080b503c81c932efd3871f776efbc48a10
Jacek Kopecky <jacek@jacek.cz>
2020-04-27 20:53:31 +0100

    stage 4

--- stages/index.js
+++ stages/index.js
@@ -1,31 +1,27 @@
 'use strict';
 /**
- * We can _structure_ the properties better, and
- * make the code a little easier to read by
- * constructing objects.
+ * We use the `class` keyword to define a class
+ * before we create instances of it (objects).
  *
- * Our `drawRect` function no longer needs
- * six parameters and the code is easier to read.
+ * We define the `Rectangle` class and create two
+ * instances of it.
+ *
+ * They are then drawn as before.
  */
 
-// create an object
-const rect1 = {
-  x: 100,
-  y: 50,
-  width: 100,
-  height: 200,
-  col: 'crimson',
-};
+class Rectangle {
+  constructor(x, y, width, height, col) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.col = col;
+  }
+}
 
-// create another object
-const rect2 = {
-  x: 300,
-  y: 150,
-  width: 100,
-  height: 200,
-  col: 'steelblue',
-};
-
+// create the two rectangle objects
+const rect1 = new Rectangle(100, 50, 100, 200, 'crimson');
+const rect2 = new Rectangle(300, 150, 100, 200, 'steelblue');
 
 // draw a rectangle
 function drawRect(c, r) {
